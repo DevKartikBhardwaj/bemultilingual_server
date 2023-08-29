3f762cbd1a6e4a5449c8ddefe5f2405a02d36e97
Kartik Bhardwaj <developer.kartikbhardwaj@gmail.com>
2023-08-29 07:32:47 +0530

    dbehwd

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.post("/add-contact-details", async (req, res) => {
     let transporter = nodemailer.createTransport(config);
     let message = {
       from: emailId,
-      to: "anujkaushik380@gmail.com",
+      to: "developer.kartikbhardwaj@gmail.com",
       subject: "Client Contact",
       html: `
       <body style="margin: 0; padding: 0; font-family: Arial, sans-serif; background-color: #f4f4f4;">
